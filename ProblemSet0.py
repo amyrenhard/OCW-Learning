7ede85b0f7563a97d7667fa303fe70bbdcc3d4fc
e30chris <chris@e30chris.me>
2015-10-14 13:16:03 -0700

    renamed file - added python interpreter

--- ProblemSet0.py
+++ ProblemSet0.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: cp1252 -*-
 
 # Problem Set 0
@@ -9,7 +10,7 @@
 ##Write a program that does the following in order:
 ##Asks the user to enter his/her date of birth.
 ##Asks the user to enter his/her last name.
-##Prints out the user’s last name and date of birth, in that order. 
+##Prints out the user’s last name and date of birth, in that order.
 ##An example of an interaction with your program is shown below (the words printed in blue are from the
 ##computer, based on your commands, the words in black are a user’s input – the colors are simply here to
 ##help you distinguish the two components):
